98b0d1660744fc8c68a7820c939ff3639f8329c1
Micah Evans <micah.s.evans@gmail.com>
2020-05-18 15:10:05 -0400

    Finished updating logic, questions, and styling

--- Master/assets/js/questions.js
+++ Master/assets/js/questions.js
@@ -1,35 +1,66 @@
 // list of all questions, choices, and answers
 var questions = [
   {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    title: "Peter Parker is inspired to become the Friendly Neighborhood Spider-man when this beloved family member is killed.",
+    choices: ["Aunt May", "Richard Parker", "Uncle Ben", "Mary Parker"],
+    answer: "Uncle Ben"
   },
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    title: "In the MCU, The Mad Titan--Thanos--is motivated by a desire for population control & balance. What is Thanos' main motivation in Marvel Comics?",
+    choices: ["The destruction of the universe", "A love for Lady Death", "Survival of the fittest", "Dominion over all beings"],
+    answer: "A love for Lady Death"
   },
   {
-    title: "Arrays in JavaScript can be used to store ____.",
+    title: "Which villain spans each of these Marvel heroes' comic series: SPIDER-MAN, DAREDEVIL, PUNISHER",
     choices: [
-      "numbers and strings",
-      "other arrays",
-      "booleans",
-      "all of the above"
+      "Rhino",
+      "King Pin",
+      "Bullseye",
+      "All of the above",
     ],
-    answer: "all of the above"
+    answer: "All of the above"
   },
   {
     title:
-      "String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    answer: "quotes"
+      "Which one of these IS NOT an Infitiy Gem? ",
+    choices: ["SOUL", "MIND", "SPIRIT", "POWER"],
+    answer: "SPIRIT"
   },
   {
     title:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-    answer: "console.log"
+      "Which of these is the original alias of Steve Rodgers' sidekick and best friend?",
+    choices: ["Bucky", "Winter Soldier", "War Machine", "U.S. Agent"],
+    answer: "Bucky"
+  },
+  {
+    title:
+      "The HULK has had a long comic history with multiple versions. Which of these is an alternate color Bruce Banner has never turned into?",
+    choices: ["Green", "Grey", "Red", "White"],
+    answer: "White"
+  },
+  {
+    title:
+      "Which of the following Marvel villians is most commonly associated with the X-Men comics?",
+    choices: ["Dr. Doom", "Dr. Octopus", "The Red Skull", "Magneto"],
+    answer: "Magneto"
+  },
+  {
+    title:
+      "Which Avenger and X-Men character was made infamous for reducing the entire mutant population?",
+    choices: ["Natasha Romanova", "Wanda Maximoff", "Franklin Richards", "Galactus"],
+    answer: "Wanda Maximoff"
+  },
+  {
+    title:
+      "Which of these IS NOT an official Marvel metal?",
+    choices: ["Adamantium", "Vibranium", "Uru", "Madentite"],
+    answer: "Madentite"
+  },
+  {
+    title:
+      "What is the name of Tony Stark's trusted friend and butler?",
+    choices: ["Edwin Jarvis", "Alfred Pennyworth", "Benson DuBois", "Reginald Jeeves"],
+    answer: "Edwin Jarvis"
   }
+
 ];
